db49254ce7bcaafc475877a91c649e9063f3d77f
technosf <github.10.technomation@xoxy.net>
2015-02-09 16:40:04 -0800

    Bind the status window text

--- src/main/java/com/github/technosf/posterer/controllers/impl/StatusController.java
+++ src/main/java/com/github/technosf/posterer/controllers/impl/StatusController.java
@@ -15,147 +15,154 @@ import com.github.technosf.posterer.controllers.AbstractController;
 import com.github.technosf.posterer.controllers.Controller;
 
 public class StatusController extends AbstractController
-        implements Controller
+				implements Controller
 {
-    /**
-     * The FXML definition of the View
-     */
-    public final static String FXML = "/fxml/Status.fxml";
+	/**
+	 * The FXML definition of the View
+	 */
+	public final static String FXML = "/fxml/Status.fxml";
 
-    private static final Logger logger = LoggerFactory
-            .getLogger(StatusController.class);
+	private static final Logger logger = LoggerFactory
+					.getLogger(StatusController.class);
 
-    /**
-     * The window title formatter
-     */
-    private final static String FORMAT_TITLE =
-            "Posterer :: Status Window";
+	/**
+	 * The window title formatter
+	 */
+	private final static String FORMAT_TITLE =
+					"Posterer :: Status Window";
 
-    private final StringBuilder status = new StringBuilder();
+	private final StringBuilder status = new StringBuilder();
 
-    /*
-     * ------------ FXML Components -----------------
-     */
+	/*
+	 * ------------ FXML Components -----------------
+	 */
 
-    @FXML
-    private TextArea statusWindow;
-
-
-    /*
-     * ------------ Statics -----------------
-     */
-
-    public static StatusController loadController(StringProperty stringProperty)
-    {
-        Stage stage = new Stage();
-        StatusController controller = null;
-
-        try
-        {
-            controller = (StatusController) StatusController
-                    .loadController(stage, FXML);
-            controller.updateStage(stringProperty);
-        }
-        catch (IOException e)
-        {
-            logger.error("Cannot load Controller.", e);
-        }
-
-        return controller;
-    }
+	@FXML
+	private TextArea statusWindow;
 
 
-    /*
-     * ------------ Code -----------------
-     */
+	/*
+	 * ------------ Statics -----------------
+	 */
 
-    /**
-     * Instantiate and set the title.
-     */
-    public StatusController()
-    {
-        super(FORMAT_TITLE); // TODO Title may not be needed
-        logger.debug("Instantiated.");
-    }
+	/**
+	 * Instantiates a StatusController, binding the given {@code StringProperty} to its text.
+	 * 
+	 * @param stringProperty
+	 * @return a new StatusControler
+	 */
+	public static StatusController loadController(StringProperty stringProperty)
+	{
+		Stage stage = new Stage();
+		StatusController controller = null;
 
+		try
+		{
+			controller = (StatusController) StatusController
+							.loadController(stage, FXML);
+			controller.updateStage(stringProperty);
+		}
+		catch (IOException e)
+		{
+			logger.error("Cannot load Controller.", e);
+		}
 
-    /**
-     * Updates this stage with event handlers
-     * 
-     * @param responseModel
-     */
-    public void updateStage(final StringProperty status)
-    {
-        //this.status = status;
-    }
-
-
-    /**
-     * {@inheritDoc}
-     * 
-     * @see com.github.technosf.posterer.controllers.Controller#initialize()
-     */
-    @Override
-    public void initialize()
-    {
-        logger.debug("Initialize.");
-    }
+		return controller;
+	}
 
 
-    /*
-     * ------------ Setters -----------------
-     */
+	/*
+	 * ------------ Code -----------------
+	 */
 
-    /**
-     * Replace the Status window text
-     * 
-     * @param message
-     *            the message to set the status window to
-     */
-    void setStatus(String message)
-    {
-        status.append(message);
-        statusWindow.setText(message);
-    }
+	/**
+	 * Instantiate and set the title.
+	 */
+	public StatusController()
+	{
+		super(FORMAT_TITLE); // TODO Title may not be needed
+		logger.debug("Instantiated.");
+	}
 
 
-    /**
-     * Replace the Status window text
-     * 
-     * @param message
-     *            the message to set the status window to
-     */
-    void setStatus(String format, Object... args)
-    {
-        setStatus(String.format(format, args));
-    }
+	/**
+	 * Updates this stage with event handlers
+	 * 
+	 * @param responseModel
+	 */
+	public void updateStage(final StringProperty status)
+	{
+		// Bind the status text property to the StatusController value
+		status.bind(statusWindow.textProperty());
+	}
 
 
-    /**
-     * Append a message to the Status window.
-     * 
-     * @param message
-     *            the message to append to the status window
-     */
-    void appendStatus(String message)
-    {
-        if (StringUtils.isNotBlank(message))
-        {
-            status.append("\n").append(message);
-            statusWindow.setText(status.toString());
-        }
-    }
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see com.github.technosf.posterer.controllers.Controller#initialize()
+	 */
+	@Override
+	public void initialize()
+	{
+		logger.debug("Initialize.");
+	}
 
 
-    /**
-     * Append a message to the Status window.
-     * 
-     * @param message
-     *            the message to append to the status window
-     */
-    void appendStatus(String format, Object... args)
-    {
-        appendStatus(String.format(format, args));
-    }
+	/*
+	 * ------------ Setters -----------------
+	 */
+
+	/**
+	 * Replace the Status window text
+	 * 
+	 * @param message
+	 *            the message to set the status window to
+	 */
+	void setStatus(String message)
+	{
+		status.append(message);
+		statusWindow.setText(message);
+	}
+
+
+	/**
+	 * Replace the Status window text
+	 * 
+	 * @param message
+	 *            the message to set the status window to
+	 */
+	void setStatus(String format, Object... args)
+	{
+		setStatus(String.format(format, args));
+	}
+
+
+	/**
+	 * Append a message to the Status window.
+	 * 
+	 * @param message
+	 *            the message to append to the status window
+	 */
+	void appendStatus(String message)
+	{
+		if (StringUtils.isNotBlank(message))
+		{
+			status.append("\n").append(message);
+			statusWindow.setText(status.toString());
+		}
+	}
+
+
+	/**
+	 * Append a message to the Status window.
+	 * 
+	 * @param message
+	 *            the message to append to the status window
+	 */
+	void appendStatus(String format, Object... args)
+	{
+		appendStatus(String.format(format, args));
+	}
 
 }
