0d34854340d0514ceb622433db480898d0dc56ee
technosf <github.10.technomation@xoxy.net>
2015-02-17 16:41:37 -0800

    KeyStore veiwer now resizes

--- src/main/java/com/github/technosf/posterer/controllers/impl/KeyStoreViewerController.java
+++ src/main/java/com/github/technosf/posterer/controllers/impl/KeyStoreViewerController.java
@@ -20,116 +20,117 @@ import com.github.technosf.posterer.controllers.Controller;
 import com.github.technosf.posterer.models.KeyStoreBean;
 
 public class KeyStoreViewerController extends AbstractController
-				implements Controller
+        implements Controller
 {
-	/**
-	 * The FXML definition of the View
-	 */
-	public final static String FXML = "/fxml/KeyStoreViewer.fxml";
+    /**
+     * The FXML definition of the View
+     */
+    public final static String FXML = "/fxml/KeyStoreViewer.fxml";
 
-	private static final Logger logger = LoggerFactory
-					.getLogger(KeyStoreViewerController.class);
+    private static final Logger logger = LoggerFactory
+            .getLogger(KeyStoreViewerController.class);
 
-	/**
-	 * The window title formatter
-	 */
-	private final static String FORMAT_TITLE =
-					"Posterer :: CertificateStoreViewer : [ %1$s ]";
+    /**
+     * The window title formatter
+     */
+    private final static String FORMAT_TITLE =
+            "Posterer :: CertificateStoreViewer : [ %1$s ]";
 
-	/*
-	 * ------------ FXML Components -----------------
-	 */
+    /*
+     * ------------ FXML Components -----------------
+     */
 
-	@FXML
-	private TextField file, type, size;
+    @FXML
+    private TextField file, type, size;
 
-	@FXML
-	private Accordion accordion;
+    @FXML
+    private Accordion accordion;
 
+    /* --------------- Storage ---------------------- */
 
-	/* --------------- Storage ---------------------- */
-
-	private KeyStoreBean keyStore;
-
-	/*
-	 * ------------ Statics -----------------
-	 */
-
-	public static Stage loadStage(KeyStoreBean keyStore)
-	{
-		Stage stage = new Stage();
-		KeyStoreViewerController controller;
-
-		try
-		{
-			controller = (KeyStoreViewerController) KeyStoreViewerController
-							.loadController(stage, FXML);
-			controller.updateStage(keyStore);
-		}
-		catch (IOException e)
-		{
-			logger.error("Cannot load Controller.", e);
-		}
-
-		return stage;
-	}
+    private KeyStoreBean keyStore;
 
 
-	/*
-	 * ------------ Code -----------------
-	 */
+    /*
+     * ------------ Statics -----------------
+     */
 
-	/**
-	 * Instantiate and set the title.
-	 */
-	public KeyStoreViewerController()
-	{
-		super(FORMAT_TITLE); // TODO Title may not be needed
-		logger.debug("Instantiated.");
-	}
+    public static Stage loadStage(KeyStoreBean keyStore)
+    {
+        Stage stage = new Stage();
+        KeyStoreViewerController controller;
+
+        try
+        {
+            controller = (KeyStoreViewerController) KeyStoreViewerController
+                    .loadController(stage, FXML);
+            controller.updateStage(keyStore);
+        }
+        catch (IOException e)
+        {
+            logger.error("Cannot load Controller.", e);
+        }
+
+        return stage;
+    }
 
 
-	/**
-	 * Updates this stage with event handlers
-	 * 
-	 * @param responseModel
-	 */
-	public void updateStage(final KeyStoreBean keyStore)
-	{
-		/*
-		 * Set the title to provide info on the HTTP request
-		 */
-		setTitle(String.format(FORMAT_TITLE, keyStore.getFileName()));
-		this.keyStore = keyStore;
-		file.setText(keyStore.getFile());
-		type.setText(keyStore.getType());
-		size.setText(Integer.toString(keyStore.getSize()));
-		List<TitledPane> panes = new ArrayList<TitledPane>();
-		for (String alias : keyStore.getAliases())
-		{
-			TextArea certificate = new TextArea(keyStore.getCertificate(alias).toString());
-			certificate.setEditable(false);
-			certificate.setWrapText(true);
-			certificate.setFont(Font.font("Monospaced", 12));
-			certificate.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-			TitledPane tp = new TitledPane(alias, certificate);
-			tp.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-			tp.setMinSize(-1, -1);
-			panes.add(tp);
-		}
+    /*
+     * ------------ Code -----------------
+     */
 
-		accordion.getPanes().setAll(panes);
-	}
+    /**
+     * Instantiate and set the title.
+     */
+    public KeyStoreViewerController()
+    {
+        super(FORMAT_TITLE); // TODO Title may not be needed
+        logger.debug("Instantiated.");
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.posterer.controllers.Controller#initialize()
-	 */
-	@Override
-	public void initialize()
-	{
-		logger.debug("Initialize.");
-	}
+    }
+
+
+    /**
+     * Updates this stage with event handlers
+     * 
+     * @param responseModel
+     */
+    public void updateStage(final KeyStoreBean keyStore)
+    {
+        /*
+         * Set the title to provide info on the HTTP request
+         */
+        setTitle(String.format(FORMAT_TITLE, keyStore.getFileName()));
+        this.keyStore = keyStore;
+        file.setText(keyStore.getFile());
+        type.setText(keyStore.getType());
+        size.setText(Integer.toString(keyStore.getSize()));
+        List<TitledPane> panes = new ArrayList<TitledPane>();
+        for (String alias : keyStore.getAliases())
+        {
+            TextArea certificate =
+                    new TextArea(keyStore.getCertificate(alias).toString());
+            certificate.setEditable(false);
+            certificate.setWrapText(true);
+            certificate.setFont(Font.font("Monospaced", 12));
+            certificate.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            TitledPane tp = new TitledPane(alias, certificate);
+            panes.add(tp);
+        }
+
+        accordion.getPanes().setAll(panes);
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.posterer.controllers.Controller#initialize()
+     */
+    @Override
+    public void initialize()
+    {
+        logger.debug("Initialize.");
+    }
 
 }
